cee8d2efbef60defeb88f2964c245c7b4d0d7649
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-10-22 16:29:59 +0100

    modified

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "../style.css";
-import Navigation from "../components/navigation";
+// import Navigation from "../components/navigation";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
     <div className="main">
       <h1>Home</h1>
-      <Navigation />
+      <div className="navigation">
+        <NavLink
+          style={({ isActive }) =>
+            isActive
+              ? {
+                  backgroundColor: "white",
+                  color: "black",
+                  border: "0.5px solid black",
+                }
+              : { color: "white" }
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink to="/users">Users</NavLink>
+      </div>
       <section>
         <img src="https://placedog.net/350/380?id=22" alt="random dog" />
       </section>
--- src/pages/users.js
+++ src/pages/users.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "../style.css";
-import Navigation from "../components/navigation";
+// import Navigation from "../components/navigation";
+import { NavLink } from "react-router-dom";
 
 function Users() {
   return (
     <div className="users">
       <h1>Users</h1>
-      <Navigation />
+      <div className="navigation">
+        <NavLink to="/">Home</NavLink>
+        <NavLink
+          style={({ isActive }) =>
+            isActive
+              ? {
+                  backgroundColor: "white",
+                  color: "black",
+                  border: "0.5px solid black",
+                }
+              : { color: "white" }
+          }
+          to="/users"
+        >
+          Users
+        </NavLink>
+      </div>
     </div>
   );
 }
