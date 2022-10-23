f9988c530c8176422015996f2a8fe49abc69dead
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-10-23 16:25:39 +0100

    added nested user page

---
+++ src/pages/user.js
@@ -0,0 +1,12 @@
+import "../style.css";
+import { Link } from "react-router-dom";
+
+function User() {
+  return (
+    <div className="user">
+      <h2>User</h2>
+    </div>
+  );
+}
+
+export default User;
