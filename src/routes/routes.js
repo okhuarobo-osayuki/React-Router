a0e738a6aa0eaddc66dad22fba51e483387b3dbb
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-10-23 16:26:42 +0100

    updated routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Users from "../pages/users";
+import User from "../pages/user";
+import ErrorFallback from "../pages/404";
 
 function RouteApp() {
-  return <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/users" element={<Users />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/users" element={<Users />}>
+        <Route path="user" element={<User />} />
+      </Route>
+      <Route path="*" element={<ErrorFallback />} />
+    </Routes>
+  );
 }
 export default RouteApp;
